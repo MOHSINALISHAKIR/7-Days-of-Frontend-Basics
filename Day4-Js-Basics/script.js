b0dd988b7fd69dc04ee5f00815ee852413441e92
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-16 23:02:19 +0500

    js

---
+++ Day4-Js-Basics/script.js
@@ -0,0 +1,44 @@
+//what is JavaScript
+//js is a programming language we use it to give instructions to the computer.
+// Variables
+// let , const , var
+// let cannot be re-declared but can be updated 
+// const  cannot be re-declared and updated
+// Data Types in Js
+//  ss bb nn u 
+//  string , symbol , boolean , bigInt , number , null , undefined
+// Operations 
+// Arithemetic and assignment and comparison operators logical  
+
+// Conditional Statements
+//  to implement some condition in the code 
+// Strings
+
+const str = 'mohsin'
+console.log(str)
+
+//Trim Method
+
+const str1 = '    hello'
+console.log(str1.trim())
+// ToUpperCase and ToLowerCase
+let case1= 'ilovecoding';
+console.log(case1.toUpperCase())
+let case2 = 'ILOVECODING';
+console.log(case2.toLowerCase())
+// Methods with Arguments
+let str2= "IloveCoding"
+console.log(str2.indexOf("love"))
+//Chaning in String
+let str3 = '     pakistan'
+console.log(str3.trim().toUpperCase())
+//Slice Method
+//return a part of string
+let str4 = 'ilovecoding';
+console.log(str4.slice(1,4))
+//Replace Method 
+let str5 = 'ilovecoding'
+console.log(str5.replace("love","do"))
+// Repeat Method
+let str6= 'pakistan'
+console.log(str6.repeat(2))
