c79858c35663f32636832e8946e52b1149d2f171
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-18 16:50:49 +0500

    object

--- Day6-Js-3/app.js
+++ Day6-Js-3/app.js
@@ -1,53 +1,80 @@
-// object Literals
-// used to store keyed collection and complex entities
+// // object Literals
+// // used to store keyed collection and complex entities
 
-const student ={
-    Name:'Mohsin',
-    Age:22,
-    Marks:92,
-    Country:'pakistan'
+// const student ={
+//     Name:'Mohsin',
+//     Age:22,
+//     Marks:92,
+//     Country:'pakistan'
+// }
+// // Get Values
+
+// console.log(student.Name)
+
+// // Add Values
+// student.City='lahore'
+
+// // Update Values
+// student.City='london'
+// // Delete Values
+// delete student.City;
+// // Nested Objects
+// const classInfo ={
+//     mohsin:{
+//         grade:"A",
+//         city:'lahore'
+//     },
+//     usman:{
+//         grade:'B',
+//         city:'london'
+//     },
+//     umar:{
+//         grade:"A+",
+//         city:'karachi'
+//     }
+// }
+// //Arrays of Objects 
+// const arrayInfo =[
+//     {
+//         name:'mohsin',
+//         grade:'A',
+//         city:'lahore'
+//     },
+//     {
+//         name:'usman',
+//         grade:'C',
+//         city:'kasur'
+//     }
+// ]
+// console.log("arrays of object")
+// console.log(arrayInfo[0].grade)
+// // console.log(classInfo.mohsin.city)
+// console.log(classInfo)
+// console.log(student)
+
+// Math Object
+
+// absolute math
+// console.log(Math.abs(-80))
+// // power funtion
+// console.log(Math.pow(3,5))
+// // Floor Function
+// console.log(Math.floor(4.8))
+// // ceiling Funtion 
+// console.log(Math.ceil(5.1))
+
+// // Random Number 
+
+// let Num = Math.floor(Math.random()*5)+1;
+// console.log(Num)
+
+let num = Math.floor(Math.random() *50 )+1;
+if(num >=10){
+    console.log(`please try again no greater than this `)
+}else if( num <= 30){
+    console.log("please try agian")
+}else if( num >=30 || num <=40){
+    console.log("congrats")
+}else{
+    console.log("error")
 }
-// Get Values
-
-console.log(student.Name)
-
-// Add Values
-student.City='lahore'
-
-// Update Values
-student.City='london'
-// Delete Values
-delete student.City;
-// Nested Objects
-const classInfo ={
-    mohsin:{
-        grade:"A",
-        city:'lahore'
-    },
-    usman:{
-        grade:'B',
-        city:'london'
-    },
-    umar:{
-        grade:"A+",
-        city:'karachi'
-    }
-}
-//Arrays of Objects 
-const arrayInfo =[
-    {
-        name:'mohsin',
-        grade:'A',
-        city:'lahore'
-    },
-    {
-        name:'usman',
-        grade:'C',
-        city:'kasur'
-    }
-]
-console.log("arrays of object")
-console.log(arrayInfo[0].grade)
-// console.log(classInfo.mohsin.city)
-console.log(classInfo)
-console.log(student)
